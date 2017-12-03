3526bc5ce28b6a26c897fb1af67e02c05fc960d3
谢三少 <xieyanjun1992@163.com>
2017-12-03 23:24:25 +0800

    增加日志文件大小判断，文件过大新加文件

--- Frame/Library/Log.class.php
+++ Frame/Library/Log.class.php
@@ -21,6 +21,12 @@ class Log
         if (!is_dir($log_path)){
             mkdir($log_path,0755,true);
         }
+
+        if (is_file($log_name) && filesize($log_name) >= 2097152){
+            $backup_name = 'backup_' . time() . '_' . basename($log_name);
+            rename($log_name,dirname($log_name) . DIRECTORY_SEPARATOR . $backup_name);
+        }
+
         $log_message = "["  . date('Y-m-d  G:i:s'). "]" . getRealIp() . " "
             . $_SERVER['REQUEST_URI'] . "\r\n" . $message . "\r\n";
         error_log($log_message,3,$log_name);
