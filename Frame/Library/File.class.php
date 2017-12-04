bd6bde45cff601fbb9367ecc0f3bf307c378a37a
谢三少 <xieyanjun1992@163.com>
2017-12-04 22:41:57 +0800

    文件处理类

--- Frame/Library/File.class.php
+++ Frame/Library/File.class.php
@@ -17,23 +17,19 @@ class File
         if (is_file($file)){
             return true;
         }
-        throw new Exception("文件为找到");
-//        }catch (Exception $e){
-//            errorOutput("文件系统错误",$e->getCode(),$e->getMessage(),$e->getFile(),$e->getLine());
-//        }
-//        return false;
+        return false;
     }
     //TODO:获取文件时是否加锁，目前对锁机制不是很了解
     public function get($file){
-        echo "123";
-        var_dump($this->isFile($file));
         if ($this->isFile($file)){
-            echo "hello world";
             return file_get_contents($file);
         }
+        return false;
     }
 
     public function getRequire($file){
-
+        if ($this->isFile($file)){
+            require $file;
+        }
     }
 }
