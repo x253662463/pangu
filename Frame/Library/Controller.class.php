52e006fb79a5a3c3c323f1c7c5ba8c4de09c4780
谢三少 <xieyanjun1992@163.com>
2017-12-27 15:48:12 +0800

    删除视图相关，框架结果已接口方式为主

--- Frame/Library/Controller.class.php
+++ Frame/Library/Controller.class.php
@@ -8,10 +8,6 @@ namespace Frame\Library;
 
 abstract class Controller
 {
-
-    //视图类
-    protected $viewData = array();
-
     /**
      * 控制器初始化操作
      * Controller constructor.
@@ -19,32 +15,10 @@ abstract class Controller
     public function __construct(){
     }
 
-    /**
-     * 将数据传入到视图中
-     * @param $data
-     */
-    public function assign($data){
-        $this->viewData = array_merge($this->viewData,$data);
-    }
-
-    /**
-     *展示视图
-     */
-    public function show(){
-//        if ($this->viewData){
-//            foreach ($this->viewData as $key => $value){
-//                $$key = $value;
-//            }
-//        }
-//        ob_start();
-//        ;
-//        if (is_file(ROOT_DIR . '/Views/' . CONTROLLER_NAME .'/'. ACTION_NAME . '.php')){
-//            include ROOT_DIR . '/Views/' . CONTROLLER_NAME .'/'. ACTION_NAME . '.php';
-//        }else{
-//            errorOutput("视图未找到",0, "未找到视图文件：" . ROOT_DIR . '/Views/' . CONTROLLER_NAME .'/'. ACTION_NAME . '.php',__FILE__,__LINE__);
-//        }
-//        $content = ob_get_clean();
-//        echo $content;
+    public function response($data){
+        ob_end_clean();
+        header('Content-type: application/json,charset=utf-8');
+        echo json_encode($data);
     }
 
 }
