41b83657c486d4a948c8f01d94fc8c29263d16c5
谢三少 <xieyanjun1992@163.com>
2018-08-16 17:07:04 +0800

    简化入口文件

--- Public/index.php
+++ Public/index.php
@@ -4,10 +4,15 @@
  * Time: 2017/11/17 11:34
  */
 
+/**
+ * 项目入口文件
+ */
 
-require '..\Frame\Library\Autoload.class.php';
+//判断系统版本不小于7.0
+if (version_compare('7.0', PHP_VERSION, '>=')) {
+    exit("need version >= 7.0.0,your version:" . PHP_VERSION);
+}
 
-new \Frame\Library\Autoload(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-
-new \Frame\App(new \Frame\Library\Router());
+//引入框架
+require __DIR__ . '/../Pangu/start.php';
 
